714c572858318211dd1e65f3bf764f1e6ffd5fd2
Lukas Hufnagel-Ben-Hadar <weblorkes@gmail.com>
2019-09-19 22:23:37 +0200

    Morphing rainbow colors added

--- sketch.js
+++ sketch.js
@@ -1,19 +1,31 @@
 let pixels = [];
 let yoff = 0;
 let offset = 0.01;
+let ROYGBIV = [];
+let lerpColors = [0,1];
+let lerpColorSpeed = 120;
 
 function setup() {
 	createCanvas(800, 800);	
+	ROYGBIV = [color(255, 0, 0),
+		color(255, 165, 0),
+		color(255, 255, 0),
+		color(0, 255, 0),
+		color(0, 0, 255),
+	 	color(75, 0, 130),
+	 	color(238, 130, 238)
+	]
 }
 
 function draw() {
   // put drawing code here
 	update();
 	
-	background(0);
+	background(51);
 	noFill();
-	stroke(255);
-	
+	stroke(changeColor());
+	strokeWeight(5);
+
 	beginShape();
 	for (let p of pixels) {
 		vertex(p.x, p.y);
@@ -33,4 +45,22 @@ function update() {
 		}
 	}
 	yoff += offset;
+}
+
+function changeColor() {
+	if (frameCount % lerpColorSpeed == 0) {
+		if (lerpColors[0] == ROYGBIV.length - 1) {
+			lerpColors[0] = 0;
+			lerpColors[1] += 1;
+		} else if (lerpColors[1] == ROYGBIV.length - 1) {
+			lerpColors[0] += 1;
+			lerpColors[1] = 0;
+		}	else {
+			lerpColors[0] += 1;
+			lerpColors[1] += 1;
+		}
+	}
+	let amt = map((frameCount % lerpColorSpeed), 0, lerpColorSpeed, 0, 1);
+	let color = lerpColor(ROYGBIV[lerpColors[0]], ROYGBIV[lerpColors[1]], amt);
+	return color;
 }
